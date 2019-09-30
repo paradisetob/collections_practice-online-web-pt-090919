fd8be3fa725aed304d2b60c29741339ede1ecf1d
tracey brimage <onelovenilynn@gmail.com>
2019-09-30 05:23:11 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
-
+array= [25,7,1]
+array.sort_array_asc
+ end
+end
+array= [25,7,14]
+array.sort_array_desc
+ end
+end
+array=["dogs", "cat", "Horses"]
+array.sort_array_char_count
+ end
+ end
+ array = ["blake", "ashley", "scott"]
+ swap_elements[1,3]
+end
+end
+array=[12, 4, 35]
+array.reverse
+end
+end
+keisha_maker = ["blake", "ashley", "scott"]
+keisha_maker.each |letter 3 to $|
+end
+end
+find_a =["apple", "orange", "pear", "avis", "arlo", "ascot" ]
+start_with? (a)
+ end
+ end
+ sum_array=11,4,7,8,9,100,134
+ sum_array.inject
+ end
+ end
+ add_s=["hand","feet", "knee", "table"]
+ each_with_index[0,2,3]
+ end
+ end
